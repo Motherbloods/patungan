12baa22823278967e3a9f10dd8fc9fedde4de2ee
Motherbloods <habibskh06@gmail.com>
2026-03-11 21:02:23 +0700

    chore(app): initialize database connection and server port

---
+++ patungan-be/app.js
@@ -0,0 +1,16 @@
+const app = require("./server");
+const connectDB = require("./src/config/db");
+const logger = require("./src/utils/logger");
+
+const PORT = process.env.PORT || 3000;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, "0.0.0.0", () => {
+      logger.info(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    logger.error(`Failed to start server: ${err.message}`);
+    process.exit(1);
+  });
