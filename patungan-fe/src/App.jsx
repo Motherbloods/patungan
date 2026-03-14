91b03af50a44b9a813f2d58f19ac4b726f92a36c
Motherbloods <habibskh06@gmail.com>
2026-03-14 13:46:07 +0700

    refactor(context): remove GroupsContext and provider from App.jsx

--- patungan-fe/src/App.jsx
+++ patungan-fe/src/App.jsx
@@ -1,25 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "./layout/MainLayout";
 import routes from "./config/routes";
-import { GroupsProvider } from "./context/GroupContext";
 
 function App() {
   return (
-    <GroupsProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<MainLayout />}>
-            {routes.map((route) => (
-              <Route
-                key={route.path}
-                path={route.path}
-                element={route.element}
-              />
-            ))}
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </GroupsProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          {routes.map((route) => (
+            <Route key={route.path} path={route.path} element={route.element} />
+          ))}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
