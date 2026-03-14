26dc984a5783ec3cfe4576996336900a156cb1c4
Motherbloods <habibskh06@gmail.com>
2026-03-15 04:45:30 +0700

    feat(service): add create, update, remove methods to expenseService

--- patungan-fe/src/services/expenseService.js
+++ patungan-fe/src/services/expenseService.js
@@ -7,6 +7,11 @@ const expenseService = {
   getTransactions: (group_id) => client.get(`/group/${group_id}/transactions`),
   getSettlements: (group_id) => client.get(`/group/${group_id}/settlements`),
   getHistory: (group_id) => client.get(`/group/${group_id}/history`),
+  create: (data) => client.post("/group/expense", data),
+  update: (group_id, expense_id, data) =>
+    client.put(`/group/${group_id}/expense/${expense_id}`, data),
+  remove: (group_id, expense_id) =>
+    client.delete(`/group/${group_id}/expense/${expense_id}`),
 };
 
 export default expenseService;
