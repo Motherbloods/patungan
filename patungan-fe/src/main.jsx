cdbe388effd6ad189d07b323318babebb1563a58
Motherbloods <habibskh06@gmail.com>
2026-03-23 13:13:08 +0700

    feat(auth): setup GoogleOAuthProvider and environment config for Google login

--- patungan-fe/src/main.jsx
+++ patungan-fe/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { GOOGLE_CLIENT_ID } from "./config/env";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient({
@@ -14,9 +16,9 @@ const queryClient = new QueryClient({
 });
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </StrictMode>,
+  </GoogleOAuthProvider>,
 );
