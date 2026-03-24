2037df8e38ba7a51f28595c24819bdbaf034f2b2
Motherbloods <habibskh06@gmail.com>
2026-03-24 12:27:43 +0700

    feat(styles): migrate Sora font to @fontsource and update global usage

--- patungan-fe/src/main.jsx
+++ patungan-fe/src/main.jsx
@@ -5,6 +5,9 @@ import App from "./App.jsx";
 import { GOOGLE_CLIENT_ID } from "./config/env";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "@fontsource/sora/300.css";
+import "@fontsource/sora/400.css";
+import "@fontsource/sora/700.css";
 
 const queryClient = new QueryClient({
   defaultOptions: {
